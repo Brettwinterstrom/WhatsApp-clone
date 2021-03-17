848ed723b7503bd4400ca7b4f3e3fbb1f0f3883e
Brett Winterstrom <brettwinterstrom@yahoo.com>
2021-03-17 14:33:41 -0700

    finished front end of whatsapp clone

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwcnHdCLUMwC4n-DVhPbGTczAQkiwLc1g",
+    authDomain: "whatsapp-clone-1f631.firebaseapp.com",
+    projectId: "whatsapp-clone-1f631",
+    storageBucket: "whatsapp-clone-1f631.appspot.com",
+    messagingSenderId: "119149111244",
+    appId: "1:119149111244:web:1d3756c79993ba741a9f69"
+};
